4df9df6ed14e2fe1a41ccbd3f859ff7a82187a14
limbo <limbosomeone@rambler.ru>
2023-05-02 12:06:29 +0500

    add new game - brain_prime.py, fix README.md, fix pyproject.toml

--- README.md
+++ README.md
@@ -4,4 +4,5 @@
 https://asciinema.org/a/HLjPckHezdLLplvrMEuZuyip4 - brain-even record  
 https://asciinema.org/a/uIxhpbfkRZclI7GCpbDv32fkt - brain-calc record  
 https://asciinema.org/a/TeaFUXfyRWF1XK17529PPSvc0 - brain-gcd record  
-https://asciinema.org/a/RnF3kSaqFm3gB8HJBCNyYoxxE = brain-progression record
+https://asciinema.org/a/RnF3kSaqFm3gB8HJBCNyYoxxE - brain-progression record
+https://asciinema.org/a/Uf65O9s4PfaFtP8NlCuOS944u - brain-prime record
---
+++ brain_games/scripts/games/brain_prime.py
@@ -0,0 +1,43 @@
+from random import uniform
+from brain_games.engine import start_brain_game
+from brain_games.cli import welcome_user
+
+
+task = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+
+def ask_prime_question():
+    question = int(uniform(1, 100))
+    for_correct_answer = question
+    return (question, for_correct_answer)
+
+
+def isPrime(n):
+    if n % 2 == 0:
+        return n == 2
+    d = 3
+    while d * d <= n and n % d != 0:
+        d += 2
+    return d * d > n
+
+
+def get_prime_correct_answer(question_result):
+    answer = isPrime(question_result)
+    if answer:
+        correct_answer = 'yes'
+    else:
+        correct_answer = 'no'
+    return correct_answer
+
+
+def start_game(name):
+    start_brain_game(task, ask_prime_question, get_prime_correct_answer, name)
+    
+    
+def main():
+    name = welcome_user()
+    start_game(name)
+    
+
+if __name__ == '__main__':
+    main()
